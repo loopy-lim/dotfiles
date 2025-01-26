8610f2974f8195a8b6df6859651e1d06c792754a
loopy-lim <loopy@jnu.ac.kr>
2025-01-26 17:50:28 +0900

    feat: add sketchybar

---
+++ sketchybar/plugins/net.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env sh
+
+# From Gruvbox Dark
+FG1=0xffebdbb2 # or whatever you use for active icons
+FG2=0xff665c54 # or whatever you use for dim icons
+
+# From SF Symbols
+NET_WIFI=􀙇         # Wi-Fi connected
+NET_HOTSPOT=􀉤      # iPhone Wi-Fi hotspot connected
+NET_USB=􀟜          # iPhone USB hotspot connected
+NET_THUNDERBOLT=􀒗  # Thunderbolt bridge connected
+NET_DISCONNECTED=􀙇 # Network disconnected, but Wi-Fi turned on
+NET_OFF=􀙈          # Network disconnected, Wi-Fi turned off
+
+# When switching between devices, it's possible to get hit with multiple
+# concurrent events, some of which may occur before `scutil` picks up the
+# changes, resulting in race conditions.
+sleep 1
+
+services=$(networksetup -listnetworkserviceorder)
+devices=$(scutil --nwi | sed -n "s/.*Network interfaces: \([^,]*\).*/\1/p")
+
+service=""
+service_index=100000
+IFS=$' ' read -ra device_array <<< "$devices"
+for device in "${device_array[@]}"; do
+  # find the service for this device
+  temp_service=$(echo "$services" | sed -n "s/.*Hardware Port: \([^,]*\), Device: $device.*/\1/p")
+  # find the string after that service in the list
+  rest=${services#*$temp_service}
+  # get the char index of the service in the list (which can be used as a priority order)
+  temp_service_index=$(( ${#services} - ${#rest} - ${#temp_service} ))
+  # if service was found and is higher priority than current service
+  if [ -n "$temp_service" ] && [ $temp_service_index -lt $service_index ]; then
+    service=$temp_service
+    service_index=$temp_service_index
+  fi
+done
+
+color=$FG1
+case $service in
+  "iPhone USB")         icon=$NET_USB;;
+  "Thunderbolt Bridge") icon=$NET_THUNDERBOLT;;
+
+  Wi-Fi)
+    ssid=$(ipconfig getsummary en0 | awk -F ' SSID : '  '/ SSID : / {print $2}')
+    case $ssid in
+      *iPhone*) icon=$NET_HOTSPOT;;
+      "")       icon=$NET_DISCONNECTED; color=$FG2;;
+      *)        icon=$NET_WIFI;;
+    esac;;
+
+  *)
+    wifi_device=$(echo "$services" \
+      | sed -n "s/.*Hardware Port: Wi-Fi, Device: \([^\)]*\).*/\1/p")
+    test -n "$wifi_device" && status=$( \
+      networksetup -getairportpower "$wifi_device" | awk '{print $NF}')
+    icon=$(test "$status" = On && echo "$NET_DISCONNECTED" || echo "$NET_OFF")
+    color=$FG2;;
+esac
+
+# sketchybar --animate sin 5 \
+sketchybar --set $NAME icon="$icon" label="$ssid" icon.color=${color}
